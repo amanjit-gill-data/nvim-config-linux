3366c17244dd206d380bd68ef39efd419231edcf
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-10-11 09:18:07 +1100

    more colour adjustments

--- plugin/nightfly.lua
+++ plugin/nightfly.lua
@@ -6,18 +6,18 @@ if not status then
 end
 
 -- source for custom bg and tints:
--- https://www.color-hex.com/color/121c26
+-- https://www.color-hex.com/color/0C0C17
 
 -- set custom bg
-vim.cmd.highlight('Normal', 'guibg=#121c26')
+vim.cmd.highlight('Normal', 'guibg=#0c0c17')
 
 -- set colour for 80-char column to a 'tint' of the custom bg
-vim.cmd.highlight('ColorColumn', 'guibg=#29323b')
+vim.cmd.highlight('ColorColumn', 'guibg=#24242e')
 
 -- remove bg from gutter and line numbering so it always matches Normal
 -- set line numbering to a 'tint' of the custom bg
 vim.cmd.highlight('SignColumn', 'guibg=NONE')
-vim.cmd.highlight('LineNr', 'guibg=NONE', 'guifg=#596067')
+vim.cmd.highlight('LineNr', 'guibg=NONE', 'guifg=#54545c')
 
 -- change string colour to a less mustardy yellow
 vim.cmd.highlight('String', 'guifg=#efd18a')
