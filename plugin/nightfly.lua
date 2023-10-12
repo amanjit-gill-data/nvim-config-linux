915ea06549ddf2c5f7f837299d329a3d707f3189
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-10-12 13:16:23 +1100

    color adjustments to suit both monitors

--- plugin/nightfly.lua
+++ plugin/nightfly.lua
@@ -9,15 +9,15 @@ end
 -- https://www.color-hex.com/color/0C0C17
 
 -- set custom bg
-vim.cmd.highlight('Normal', 'guibg=#0c0c17')
+vim.cmd.highlight('Normal', 'guibg=#181b1f')
 
 -- set colour for 80-char column to a 'tint' of the custom bg
-vim.cmd.highlight('ColorColumn', 'guibg=#24242e')
+vim.cmd.highlight('ColorColumn', 'guibg=#2f3135')
 
 -- remove bg from gutter and line numbering so it always matches Normal
 -- set line numbering to a 'tint' of the custom bg
 vim.cmd.highlight('SignColumn', 'guibg=NONE')
-vim.cmd.highlight('LineNr', 'guibg=NONE', 'guifg=#54545c')
+vim.cmd.highlight('LineNr', 'guibg=NONE', 'guifg=#5d5f62')
 
 -- change string colour to a less mustardy yellow
 vim.cmd.highlight('String', 'guifg=#efd18a')
---
+++ plugin/nvim-tree.lua
@@ -0,0 +1,15 @@
+local status, nvimtree = pcall(require, "nvim-tree")
+if not status then
+  return
+end
+
+-- disable builtin file explorer
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+nvimtree.setup({
+  git = {
+    enable = true
+  }
+})
+
