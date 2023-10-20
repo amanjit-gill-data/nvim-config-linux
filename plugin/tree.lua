c1dee98f73f82e2d5c387706a16bd4b4162d16d9
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-10-20 18:32:36 +1100

    minor edit nvim-tree config

--- plugin/tree.lua
+++ plugin/tree.lua
@@ -7,13 +7,5 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup({
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false
-      },
-    },
-  }
-})
+nvimtree.setup()
 
