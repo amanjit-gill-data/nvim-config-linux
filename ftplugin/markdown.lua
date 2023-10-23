ab9c117d9aa72fc8ea863dae2ca7b2034e7306af
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-10-23 18:21:00 +1100

    add clipboard paster for markdown

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,10 @@
--- settings for markdown files
+function setup_paster()
+  vim.cmd('packadd clipboard-image.nvim')
+  require'clipboard-image'.setup()
+end
+
+-- protected call to set up clipboard plugin, but proceed with config if fails
+local status, _ = pcall(setup_paster)
 
 local opt = vim.opt
 
