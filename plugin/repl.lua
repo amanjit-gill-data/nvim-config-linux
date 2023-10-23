4a965c12de02e5c092b6a39bc5dc891e6ca3718b
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-10-23 12:11:01 +1100

    improve repl keymap

--- plugin/repl.lua
+++ plugin/repl.lua
@@ -11,10 +11,13 @@ vim.g.repl_filetype_commands = {
 }
 
 vim.keymap.set('n', '<leader><leader>e', ':ReplToggle<CR>', { noremap = true })
+
+-- cell marker is defined by repl as #%%
 vim.keymap.set('n', '<leader>ec', ':ReplRunCell<CR>', { noremap = true })
 
 -- select paragraph (e.g. sql query, bash block, etc) and send to repl
-vim.keymap.set("n", "<leader>eq", "vip:ReplSendVisual<CR>")
+-- move to next paragraph (assumes separated by a single empty line)
+vim.keymap.set("n", "<leader>eq", "vip:ReplSendVisual<CR>vip<Esc>jj")
 
 
 
