6ed5b08317c1acc7f9719017b6167ed8aabef454
amanjit-gill-data <amanjitgilldata@gmail.com>
2023-11-23 17:59:59 +1100

    fix repl config to reflect plugin update

--- plugin/repl.lua
+++ plugin/repl.lua
@@ -5,18 +5,21 @@ vim.opt.shell = 'cmd'
 vim.g.repl_default = 'powershell bash --login'
 
 vim.g.repl_filetype_commands = {
-  python = 'ipython', -- no-autoindent is already set in ipython config 
+  python = 'ipython', -- no-autoindent already set in ipython config 
   sql = 'mariadb'
 }
 
 vim.cmd("command PySpark ReplOpen pyspark")
 
-vim.keymap.set('n', '<leader><leader>e', ':ReplToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<leader><leader>e', '<Cmd>ReplToggle<CR>', { noremap = true })
 
 -- cell marker is defined by repl as # %%
-vim.keymap.set("n", "nc", "i# %% ")
-vim.keymap.set('n', '<leader>ec', ':ReplRunCell<CR>', { noremap = true })
+vim.keymap.set("n", "nc", "i<CR># %%<CR>")
+vim.keymap.set('n', "<leader>ec", "<Plug>ReplSendCell")
+
+-- send line (e.g. pyspark method) to repl 
+vim.keymap.set("n", "<leader>el", "<Plug>ReplSendLine")
 
 -- send paragraph (e.g. sql query, bash block) to repl
-vim.keymap.set("n", "<leader>eq", "vip:ReplSendVisual<CR>")
+vim.keymap.set("n", "<leader>ep", "vip<Plug>ReplSendVisual<Esc>vipjj<Esc>")
 
